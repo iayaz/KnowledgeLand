38f7d5ee0eeaec01ae3194c822712a7f305e0257
iayaz1 <iamlordayaz@gmail.com>
2023-12-19 13:48:09 +0530

    Updated the typo in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter, Navigate,Routes, Route } from "react-router-dom";
-import HomePage from './scenes/homePage/index.js'
-import LoginPage from 'scenes/loginPage'
-import ProfilePage from 'scenes/ProfilePage'
+
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from 'scenes/homePage/index.jsx';
+import LoginPage from 'scenes/loginPage/index.jsx';
+import ProfilePage from 'scenes/profilePage/index.jsx';
+
 
 function App() {
-  const mode = use
-  return <div className="app">
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage/>}/>
-        <Route path="/home" element={<HomePage/>}/>
-        <Route path="/profile/:userId" element={<ProfilePage/>}/>
 
-
-      </Routes>
-    </BrowserRouter>
-  </div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
