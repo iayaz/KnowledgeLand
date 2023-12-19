9201a86bb31fb5e6725e1addb2d9155ee8aeb32d
iayaz1 <iamlordayaz@gmail.com>
2023-12-19 16:56:35 +0530

    changes directory structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import HomePage from 'scenes/homePage/index.jsx';
-import LoginPage from 'scenes/loginPage/index.jsx';
-import ProfilePage from 'scenes/profilePage/index.jsx';
+import HomePage from './scenes/homePage/index.jsx';
+import LoginPage from './scenes/loginPage/index.jsx';
+import ProfilePage from './scenes/profilePage/index.jsx';
 
 
 function App() {
@@ -11,9 +11,9 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
+          <Route path="/" element={<LoginPage/>} />
+          <Route path="/home" element={<HomePage/>} />
+          <Route path="/profile/:userId" element={<ProfilePage/>} />
         </Routes>
       </BrowserRouter>
     </div>
